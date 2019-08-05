21478f5cd95b5b88a775f8ba31ec4a940c5f1006
Julianb12 <julianb1212@gmail.com>
2019-08-05 11:16:51 -0500

    Header added

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = () => { 
+  return ( 
+
+<div className="App-header">
+<header> 
+  <h1>Google Book Search</h1> 
+</header>
+</div> ) }
+
+export default Header
